8982d351c62f5d1c898d3cac19f918e99b47b189
pythonmonk <saebyeok.kim@suprime.co.kr>
2016-01-10 23:05:44 +0900

    auto login auto writing

--- web_crawler.py
+++ web_crawler.py
@@ -1,33 +1,108 @@
 #-*- coding: utf-8 -*-
-import cookielib, urllib2, urllib
-
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-urllib2.install_opener(opener)
-
-params = urllib.urlencode({"mode":"login", "pixiv_id":"hwanisgood", "pass":"suprimeisgood"})
-req = urllib2.Request("http://www.naver.com", params)
-
-data = urllib.urlopen('http://cafe.naver.com/joonggonara')
-#decode
-result = data.decode('utf-8')
-#html = res.read()
-print result.read()
-print ('테스트');
-
-# "www.daum.net" 로그인
 import urllib
 import urllib2
+import BeautifulSoup
 import ClientCookie
+from ClientForm import ParseResponse
 
-form = { id: '아이디', pw: '비밀번호 }
-# 파라미터
-qstring = urllib.urlencode(form)
-# 다음 로그인 페이지
-request = urllib2.Request('https://logins.daum.net/Mail-bin/login.cgi?%s' % qstring)
+#naver login
+url_login='https://nid.naver.com/nidlogin.login'
+url_cafe='http://cafe.naver.com/wkqdbsxo14'
+url_write='http://cafe.naver.com/ArticleWrite.nhn'
+url_list='http://cafe.naver.com/ArticleList.nhn'
+
+forms = ParseResponse(urllib2.urlopen(url_login))
+form = forms[0]
+
+#print 'enter naver id'
+form['id'] = 'suprimeisgood'
+#print 'enter naver password'
+#form['pw'] = raw_input()
+form['pw'] = 'sjwkfskTek0'
+
+request = form.click()
 response = ClientCookie.urlopen(request)
 
-# 로그인 되었는지 확인할 사용자 정보 페이지
-request = urllib2.Request('https://user.daum.net/daumuser/loginUserInfo.daum?t__nil_loginbox=modify')
-response = ClientCookie.urlopen(request)
-print response.read()
+
+
+#open naver cafe
+
+response = urllib2.urlopen(url_cafe)
+
+#get cafe clubid
+#parsing
+#assign clubid and menuid
+
+soup = BeautifulSoup.BeautifulSoup(response)
+forms = soup.findAll('form', attrs={'name':'notifrm'})
+clubid = forms[0].find('input')['value']
+menuid = '433'
+
+title = 'model title'
+content = 'model body'
+referer = url_list+"?search.clubid="+clubid+"&search.menuid="+menuid+"&search.boardtype=L#"
+#put title & content in form
+#body = str(form)
+body='temp'
+
+#Request ArticleWrite.nhn
+
+#add header clubid, menuid
+#add body
+#request
+#referer = ArticleList.nhn
+url='http://cafe.naver.com/ArticleWrite.nhn'
+
+data = {'search.clubid':clubid, 'search.menuid':menuid,'search.boardtype':'L#'}
+data = urllib.urlencode(data)
+
+req = urllib2.Request(url, data)
+
+req.add_header("referer", referer)
+
+req.add_data(body)
+
+response = urllib2.urlopen(req)
+#Request TempsavePost.nhn
+
+#add header clubid, menuid, referer
+#add body
+#request
+
+#referer = ArticleWrite
+url='http://cafe.naver.com/TempsavePost.nhn'
+
+data = {'search.clubid':clubid, 'search.menuid':menuid,'search.boardtype':'L#'}
+data = urllib.urlencode(data)
+
+req = urllib2.Request(url, data)
+
+referer = url_write+"?search.clubid="+clubid+"&search.menuid="+menuid+"&search.boardtype=L#"
+req.add_header("referer", referer)
+
+req.add_data(body)
+
+response = urllib2.urlopen(req)
+#Request ArticlePost.nhn
+#add header clubid, menuid, referer
+#add body
+#request
+
+#referer = ArticleWrite
+url='http://cafe.naver.com/ArticlePost.nhn'
+
+data = {'search.clubid':clubid, 'search.menuid':menuid,'search.boardtype':'L#'}
+data = urllib.urlencode(data)
+
+req = urllib2.Request(url, data)
+
+req.add_header("referer", referer)
+
+req.add_data(body)
+
+response = urllib2.urlopen(req)
+print 'last'
+
+#board list add/delete
+#writing attribute picture
+#writing delay
