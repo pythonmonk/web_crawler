c9a0adc5b3a64962a7fe551390deabf35c50c8d8
pythonmonk <saebyeok.kim@suprime.co.kr>
2016-01-17 00:04:13 +0900

    2016-01-17 auto login auto writing

--- web_crawler.py
+++ web_crawler.py
@@ -1,108 +1,47 @@
 #-*- coding: utf-8 -*-
-import urllib
-import urllib2
+import urllib, urllib2
 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import ClientCookie
 from ClientForm import ParseResponse
 
-#naver login
 url_login='https://nid.naver.com/nidlogin.login'
 url_cafe='http://cafe.naver.com/wkqdbsxo14'
-url_write='http://cafe.naver.com/ArticleWrite.nhn'
+url_write='http://cafe.naver.com/ArticleWrite.nhn?clubid=14001943&menuid=433&boardtype=&page=1&specialmenutype=&userDisplay=15&m=write'
 url_list='http://cafe.naver.com/ArticleList.nhn'
 
-forms = ParseResponse(urllib2.urlopen(url_login))
-form = forms[0]
 
-#print 'enter naver id'
-form['id'] = 'suprimeisgood'
-#print 'enter naver password'
-#form['pw'] = raw_input()
-form['pw'] = 'sjwkfskTek0'
+# 네이버 로그인 주소
+id = 'suprimeisgood'
+pw = 'sjwkfskTek0'
+#xpaths = {'id': "//input[@name='id']", 'pw':"//input[@name='pw']"}
 
-request = form.click()
-response = ClientCookie.urlopen(request)
+xpaths = {'id': "//input[@name='id']", 'pw':"//input[@name='pw']"}
+
+#mydriver = webdriver.Firefox()
+mydriver=webdriver.PhantomJS(executable_path=r'C:\Python27\phantomjs\bin\phantomjs.exe')
+# 파이어폭스 path 어쩌고 에러가 나면서 못 찾는다면 일단 본인의 서버에 FireFox를 설치해보자
+# (참고 파이어폭스 설치)
+
+mydriver.get(url_login)
+mydriver.find_element_by_xpath(xpaths['id']).send_keys(id)
+mydriver.find_element_by_xpath(xpaths['pw']).send_keys(pw)
+btn = mydriver.find_element_by_css_selector('.int_jogin')
 
 
+btn.click()
+cafe=mydriver.find_element_by_id('svc.cafe')
+cafe.click()
+#cafe=mydriver.find_element_by_css_selector("a[href='http://cafe.naver.com/wkqdbsxo14']")
 
-#open naver cafe
+#cafe.click()
+mydriver.get(url_write)
+subject=mydriver.find_element_by_name('subject')
+subject.clear()
+subject.send_keys('model')
+mydriver.find_element_by_name("content")
 
-response = urllib2.urlopen(url_cafe)
-
-#get cafe clubid
-#parsing
-#assign clubid and menuid
-
-soup = BeautifulSoup.BeautifulSoup(response)
-forms = soup.findAll('form', attrs={'name':'notifrm'})
-clubid = forms[0].find('input')['value']
-menuid = '433'
-
-title = 'model title'
-content = 'model body'
-referer = url_list+"?search.clubid="+clubid+"&search.menuid="+menuid+"&search.boardtype=L#"
-#put title & content in form
-#body = str(form)
-body='temp'
-
-#Request ArticleWrite.nhn
-
-#add header clubid, menuid
-#add body
-#request
-#referer = ArticleList.nhn
-url='http://cafe.naver.com/ArticleWrite.nhn'
-
-data = {'search.clubid':clubid, 'search.menuid':menuid,'search.boardtype':'L#'}
-data = urllib.urlencode(data)
-
-req = urllib2.Request(url, data)
-
-req.add_header("referer", referer)
-
-req.add_data(body)
-
-response = urllib2.urlopen(req)
-#Request TempsavePost.nhn
-
-#add header clubid, menuid, referer
-#add body
-#request
-
-#referer = ArticleWrite
-url='http://cafe.naver.com/TempsavePost.nhn'
-
-data = {'search.clubid':clubid, 'search.menuid':menuid,'search.boardtype':'L#'}
-data = urllib.urlencode(data)
-
-req = urllib2.Request(url, data)
-
-referer = url_write+"?search.clubid="+clubid+"&search.menuid="+menuid+"&search.boardtype=L#"
-req.add_header("referer", referer)
-
-req.add_data(body)
-
-response = urllib2.urlopen(req)
-#Request ArticlePost.nhn
-#add header clubid, menuid, referer
-#add body
-#request
-
-#referer = ArticleWrite
-url='http://cafe.naver.com/ArticlePost.nhn'
-
-data = {'search.clubid':clubid, 'search.menuid':menuid,'search.boardtype':'L#'}
-data = urllib.urlencode(data)
-
-req = urllib2.Request(url, data)
-
-req.add_header("referer", referer)
-
-req.add_data(body)
-
-response = urllib2.urlopen(req)
-print 'last'
-
-#board list add/delete
-#writing attribute picture
-#writing delay
+btn=mydriver.find_element_by_id('cafewritebtn')
+btn.click()
+print mydriver.title
